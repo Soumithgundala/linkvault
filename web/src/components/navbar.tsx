19e85362599f0ed1fa8972066c53c17d634f3fbe
soumithgundala <soumithgundala@gmail.com>
2025-04-05 23:53:01 +0530

    done for today 05-04

---
+++ web/src/components/navbar.tsx
@@ -0,0 +1,26 @@
+// src/components/Navbar.tsx
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="max-w-7xl mx-auto px-4 py-3 flex justify-between items-center">
+        <div className="navbar-logo">LinkVault</div>
+        <div className="space-x-4">
+          <Link
+            href="/signup"
+            className="navbar-button navbar-button-signup"
+          >
+            Sign Up
+          </Link>
+          <Link
+            href="/login"
+            className="navbar-button navbar-button-login"
+          >
+            Login
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
