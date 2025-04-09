c8572d3524f2717fdbd726f95f46c5607228cfd5
soumithgundala <soumithgundala@gmail.com>
2025-04-09 16:09:56 +0530

    fixed dropdown button issue

--- web/src/components/navbar.tsx
+++ web/src/components/navbar.tsx
@@ -28,6 +28,7 @@ function Navbar() {
         <div className="logo-container" onClick={() => window.location.href = "/"}>
           <img src="/linkvault-logo.svg.png" alt="LinkVault Logo" className="logo" />
         </div>
+        <h1 className="Title-Linkvault">LinkVault</h1>
         <div className="dropdown-button" onClick={handleDropdown}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" className="dropdown-icon">
             <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
